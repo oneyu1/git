e2f8ac4415661b69b3d6b2a3d2d0d3dcc60f34e4
yi <university080@gmail.com>
2017-09-04 18:19:11 +0900

    test

---
+++ app/index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>コントロールサンプル</title>
+  </head>
+  <body>
+<a href="http://localhost/app/insert.php">http://localhost/app/insert.php</a>
+<br><a href="http://localhost/app/search.php">http://localhost/app/search.php</a>
+  </body>
+</html>
---
+++ app/insert.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>大規模テスト</title>
+  </head>
+  <body>
+      <form action="app/" method="post">
+      名前:<input type="text" name="Name"><br>
+      生年月日:<select name="cmbList"></select><br>
+      種別:<input type="radio" name="rdoSample"><br>
+      電話番号:<input type="text" name="tell"><br>
+      自己紹介:<input type="text" name="appeal"><br>
+    </form>
+  </body>
+</html>
